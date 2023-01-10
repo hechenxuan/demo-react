fe0b8486600c1e262229cec36ba121f7f9dbc998
hechenxuan <489589868@qq.com>
2023-01-10 20:43:43 +0800

    modify11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-console.log('init sdada ')
 ReactDOM.render(<div>
   <p>888888</p>
+  <b>dkasjkdjask</b>
 </div>,document.getElementById('root'))
